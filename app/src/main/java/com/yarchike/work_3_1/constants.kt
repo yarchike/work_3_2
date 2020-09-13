ecc3bd23aeb1d36047fa95afe0403e470841638b
Yarchik <yarchike@gmail.com>
2020-09-13 16:19:30 +0300

    Поменял сервер

--- app/src/main/java/com/yarchike/work_3_1/constants.kt
+++ app/src/main/java/com/yarchike/work_3_1/constants.kt
@@ -2,8 +2,8 @@ package com.yarchike.work_3_1
 
 const val AUTHENTICATED_SHARED_KEY = "authenticated_shared_key"
 const val API_SHARED_FILE = "API_shared_file"
-//const val BASE_URL = "https://server-martynov.herokuapp.com"
-const val BASE_URL = "http://93.179.85.126:5050"
+const val BASE_URL = "https://server-martynov.herokuapp.com"
+//const val BASE_URL = "http://93.179.85.126:5050"
 const val FIRST_TIME_SHARED_KEY = "first_time_shared_key"
 const val LAST_TIME_VISIT_SHARED_KEY = "last_time_visit_shared_key"
 const val SHOW_NOTIFICATION_AFTER_UNVISITED_MS:Long = 10*60*1000
